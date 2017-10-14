f9c059e2385855a55fd859b80f21a5c0e2dfbe54
Paul <paulll.lucas@gmail.com>
2017-10-14 03:44:53 -0700

    Boot -> Lein

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject natureofcode "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [quil "2.6.0"]]
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"})
